0aa79a92a6e11e168b4e468be3e1310a9f2c43d1
Hesam <102475334+hesam0987HHe@users.noreply.github.com>
2023-05-12 14:00:16 +0330

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,12 @@
 let dark = document.getElementById('darkmod');
-
+let darkTheme = true
 dark.addEventListener('click' , function(){
-	document.dataTheme = ''
+	if (darkTheme) {
+		document.getElementById('hh').setAttribute('data-theme','black')
+		darkTheme = false
+	} else {
+		document.getElementById('hh').setAttribute('data-theme','cupcake')
+		darkTheme = true
+	}
+	
 })
